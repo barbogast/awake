080b1fa3ff02cd7176b892df059d5e6138b1f018
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-01-13 10:08:27 +0100

    Move class Pos to separate file

--- src/index.ts
+++ src/index.ts
@@ -51,41 +51,6 @@ function drawRect(
   ctx.fillRect(pos.x - s, pos.y - s, size, size);
 }
 
-class Pos {
-  constructor(x: number, y: number) {
-    // @ts-ignore
-    this.vector = new Vector(x, y);
-  }
-
-  get x(): number {
-    // @ts-ignore
-    return this.vector["0"];
-  }
-
-  get y(): number {
-    // @ts-ignore
-    return this.vector["1"];
-  }
-
-  set x(value) {
-    // @ts-ignore
-    this.vector = new Vector(value, this.y);
-  }
-
-  set y(value) {
-    // @ts-ignore
-    this.vector = new Vector(this.x, value);
-  }
-
-  equals(pos: Pos) {
-    return this.x === pos.x && this.y === pos.y;
-  }
-
-  clone() {
-    return new Pos(this.x, this.y);
-  }
-}
-
 type Hitbox = {
   x: [number, number];
   y: [number, number];
---
+++ src/pos.ts
@@ -0,0 +1,34 @@
+class Pos {
+  constructor(x: number, y: number) {
+    // @ts-ignore
+    this.vector = new Vector(x, y);
+  }
+
+  get x(): number {
+    // @ts-ignore
+    return this.vector["0"];
+  }
+
+  get y(): number {
+    // @ts-ignore
+    return this.vector["1"];
+  }
+
+  set x(value) {
+    // @ts-ignore
+    this.vector = new Vector(value, this.y);
+  }
+
+  set y(value) {
+    // @ts-ignore
+    this.vector = new Vector(this.x, value);
+  }
+
+  equals(pos: Pos) {
+    return this.x === pos.x && this.y === pos.y;
+  }
+
+  clone() {
+    return new Pos(this.x, this.y);
+  }
+}
