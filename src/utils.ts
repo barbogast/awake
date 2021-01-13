e0c9038629930c30994c32957d7692e30986d39e
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-01-13 10:09:39 +0100

    Move util functions to separate file

---
+++ src/utils.ts
@@ -0,0 +1,49 @@
+function getRandomArbitrary(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomPos() {
+  return new Pos(getRandomArbitrary(1, WIDTH), getRandomArbitrary(1, HEIGHT));
+}
+
+function chunkArray<T>(sourceArray: T[], chunkSize: number): T[][] {
+  let innerArray = [];
+  const outerArray = [];
+
+  for (const el of sourceArray) {
+    innerArray.push(el);
+    if (innerArray.length === chunkSize) {
+      outerArray.push(innerArray);
+      innerArray = [];
+    }
+  }
+  if (innerArray.length !== chunkSize) {
+    outerArray.push(innerArray);
+  }
+
+  return outerArray;
+}
+
+function drawCircle(
+  ctx: CanvasRenderingContext2D,
+  pos: Pos,
+  color: string,
+  radius: number
+) {
+  ctx.beginPath();
+  ctx.arc(pos.x, pos.y, radius, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawRect(
+  ctx: CanvasRenderingContext2D,
+  pos: Pos,
+  color: string,
+  size: number
+) {
+  ctx.fillStyle = color;
+  const s = size / 2;
+  ctx.fillRect(pos.x - s, pos.y - s, size, size);
+}
